a381ec2dcb9f716301b87e4d28eb29600e916d1e
adijus <adi.jusic03@outlook.de>
2021-10-03 20:07:18 +0200

    Aufgabe Nr. 11/12 erledigt

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -13,36 +13,39 @@ using namespace std;
 
 char charout();
 
-void func(int signum){
-    wait(NULL);
-}
 
 int main(){
+    cout << "waiting for 3 seconds" << endl;
     pid_t pid;
     pid = fork();
-    char s1, s2;
-    if(errno == ENOSYS){
-        cout << "log(-l) failed: " << errno << endl;
-    } else {
-        s1 = charout();
-        s2 = charout();
-    }
-    
+    //char s1, s2;
+    //if(errno == ENOSYS){
+    //    cout << "log(-l) failed: " << errno << endl;
+    //} else {
+    //    s1 = charout();
+    //    s2 = charout();
+    //}
     for(int j = 0; j < 6; j++){
-
         if(pid == 0){
-            cout << s1;
+            cout << "A" << flush;
 
-        } else {
-            cout << s2;
-            kill(pid, SIGCHLD);
+        } else if (pid > 0) {
+            cout << "B" << flush;
         }
         chrono::milliseconds sleeptime(500);
         this_thread::sleep_for(sleeptime);
-        pid = fork();
     }
+
+    auto pid_tmp = pid;
+
+    cout << endl << "killing subprocesses with pid " << pid_tmp << endl;
+    cout << "waiting for subprocesses to be dead" << endl;
+
+    kill(pid, SIGKILL);
+
+    cout << "subprocess " << pid_tmp << " exited with 0" << endl;
+
     exit(EXIT_SUCCESS);
 
-    cout << endl;
     return 0;
 }
