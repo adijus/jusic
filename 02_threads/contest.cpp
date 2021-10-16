82cd11f91fdf2ad31809a4b15298ef3a225abecd
adijus <adi.jusic03@outlook.de>
2021-10-16 20:37:19 +0200

    Print-Ausgabe verändert und zufällige Rundenzeit hinzugefügt

--- 02_threads/contest.cpp
+++ 02_threads/contest.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <chrono>
+#include <functional>
+#include <random>
 
 using namespace std;
 
@@ -26,39 +28,52 @@ class Car{
 
         int getVelocity(){
             return velocity;
-        }
+        }  
 
-        Car(string m, int velo){
+        void lada_taiga(string mark, int v){
+            int rounds = 1;
+            random_device rd;
+
+            while(true){
+                mt19937 gen{rd()};
+                uniform_real_distribution<> dis{1, 10};
+                uniform_real_distribution<> kmh{50, 130};
+                
+                cout << to_string(rounds) << flush;
+                cout << " / " << flush;
+                cout << mark << flush;
+                cout << " " << kmh(gen) << "km/h" << flush;
+                cout << " " << dis(gen) << "s" << flush;
+                cout << endl;
+                
+                this_thread::sleep_for(1s);
+                rounds++;
+            }
+        }
+        
+        void operator()(string m, int velo){
             setMark(m);
             setVelocity(velo);
             cout << "insert Car " << getMark() << " in Database" << endl;
+            lada_taiga(getMark(), getVelocity());
+        }
 
-        }     
-        
         void print(){
-            cout << "Car: " << mark << " / Velocity: " << velocity << endl;
+            cout << "Car: " << mark << flush;
+            cout << " / Velocity: " << to_string(velocity) << flush;
+            cout << endl;
         }
 
-        void operator()(){
-            
-        }
 };
 
-
-void lada_taiga(string mark, int v){
-    int rounds = 1;
-    while(true){
-        cout << "Rounds: " << rounds << " / " << mark << " " << v << "km/h" << endl;
-        this_thread::sleep_for(1s);
-        rounds++;
-    }
-}
 
 
 int main(){
     //lada_taiga("JET-SLI-Cabrio", 200);
-    Car c1{"JET-SLI-Cabrio", 200};
-    Car c2{"Opel", 400};
-    c2.print();
-    c1.print();
+    Car car1, car2;
+    car1("JET-SLI-Cabrio", 200);
+    //car2("Opel", 400);
+    car1.print();
+    //car2.print();
+
 }
