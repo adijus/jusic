3db0015102d6dd0d6ff2e0137a31a52fe0378052
adijus <adi.jusic03@outlook.de>
2021-11-26 11:20:41 +0100

    Kommandozeile Abfrage implementiert

--- 04_sync2/src/main.cpp
+++ 04_sync2/src/main.cpp
@@ -5,9 +5,25 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
 
-    mutex m;
+    if(string(argv[1]) == ""){
+        cout << "size is required" << endl;
+        cout << "Run with --help for more information" << endl;
+        return 0;
+    } else if (string(argv[1]) == "-h"){
+        cout << "Boss and worker simulation" << endl;
+        cout << "Usage: loadsim [OPTIONS] size" << endl << endl;
+        cout << "Positionals:" << endl;
+        cout << "  size UNIT REQUIRED\tSize of the queue" << endl << endl;
+        cout << "Options:" << endl;
+        cout << "  -h,--help\t\tPrint help this message and exit" << endl;
+        return 0;
+    } else if (atoi(argv[1]) < 0){
+        cout << "Could not convert: size = " << argv[1] << endl;
+        cout << "Run with --help for more information" << endl;
+        return 0;
+    }
     WorkQueue q2(2);
     thread t1{[&]{WorkQueue q1(1);}};
     thread t2{[&]{WorkQueue q2(2);}};
