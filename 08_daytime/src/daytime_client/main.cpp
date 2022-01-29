9282e0f6cdc1a634deea41ac9904a8d3cde1cbe8
adijus <adi.jusic03@outlook.de>
2022-01-29 18:24:22 +0100

    add Client to Project

--- 08_daytime/src/daytime_client/main.cpp
+++ 08_daytime/src/daytime_client/main.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include "spdlog/spdlog.h"
+#include "asio.hpp"
+#include <typeinfo>
 
 using namespace std;
+using namespace asio::ip;
 
-int main(){
-    spdlog::info("Welcome to spdlog!");
-    spdlog::error("Some error message with arg: {}", 1);
+int main(int argc, char* argv[]){
+    if(argc == 1){
+        tcp::iostream tcp_connection{"localhost", "1113"};
+        if(tcp_connection){
+            string data;
+            getline(tcp_connection, data);
+            cout << data << endl;
+            tcp_connection.close();
+        } else {
+            cerr << "Could not Connect to the Server!" << endl;
+        }
+    } else if(argc == 2) {
+        cout << "yes" << endl;
+        string port = argv[1];
+        tcp::iostream tcp_connection{"localhost", port};
+        if(tcp_connection){
+            string data;
+            getline(tcp_connection, data);
+            cout << data << endl;
+            tcp_connection.close();
+        } else {
+            cerr << "Could not Connect to the Server!" << endl;
+        }
+    }
     
-    spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    spdlog::info("Support for floats {:03.2f}", 1.23456);
-    spdlog::info("Positional args are {1} {0}..", "too", "supported");
-    spdlog::info("{:<30}", "left aligned");
     
-    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-    spdlog::debug("This message should be displayed..");    
-    
-    // change log pattern
-    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-    
-    // Compile time log levels
-    // define SPDLOG_ACTIVE_LEVEL to desired level
-    SPDLOG_TRACE("Some trace message with param {}", 42);
-    SPDLOG_DEBUG("Some debug message");
 }
--- 08_daytime/src/daytime_server/main.cpp
+++ 08_daytime/src/daytime_server/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include "spdlog/spdlog.h"
+#include "asio.hpp"
+#include <typeinfo>
 
 using namespace std;
+using namespace asio::ip;
 
-int main(){
-    return 0; 
+int main(int argc, char* argv[]){
+    if(argc == 1){
+        tcp::iostream tcp_connection{"localhost", "1113"};
+        if(tcp_connection){
+            string data;
+            getline(tcp_connection, data);
+            cout << data << endl;
+            tcp_connection.close();
+        } else {
+            cerr << "Could not Connect to the Server!" << endl;
+        }
+    } else if(argc == 2) {
+        cout << "yes" << endl;
+        string port = argv[1];
+        tcp::iostream tcp_connection{"localhost", port};
+        if(tcp_connection){
+            string data;
+            getline(tcp_connection, data);
+            cout << data << endl;
+            tcp_connection.close();
+        } else {
+            cerr << "Could not Connect to the Server!" << endl;
+        }
+    }
+    
+    
 }
